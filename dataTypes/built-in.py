946518834af1fdb6cfbe157e7e5150dc0e034baf
IronZiiz <thiagozilli10@gmail.com>
2022-09-21 10:58:18 -0300

    Learning about Data types 21/09/2022

---
+++ dataTypes/built-in.py
@@ -0,0 +1,28 @@
+"""
+Text type : str 
+Numeric Types: int, float, complex 
+Sequence Types: list, tuple, range
+Mapping Type: dict 
+Set Types: set, frozenset
+Boolean Type: bool 
+Binary Types: bytes, bytearray, memoryview
+None Type: NoneType 
+"""
+
+#Getting the Data type 
+x = 666
+print(type(x))
+
+#Setting the Data Type 
+    #The data type is sey when you assing a value to a variale
+    #exemples 
+
+a = "Hello"
+b = 123 
+
+
+#setting specific Data type
+a = str("Hello")
+b = int(666)
+c = float(3.14)
+d = complex(666j)
---
+++ dataTypes/dataTypesExecises/ex12_typeOfX5.py
@@ -0,0 +1,13 @@
+#12)The following code example would print the data type of x, what data type would that be?
+
+"""
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+tuple
+"""
+
+#What is tuple 
+#Tuples are used to store multiple items in a single variable.
+#A tuple is a collection which is ordered and unchangeable.
+
