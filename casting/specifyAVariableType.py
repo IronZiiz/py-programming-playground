0b6af95d29d98c861e708f71da67d7c5b9861c21
IronZiiz <thiagozilli10@gmail.com>
2022-09-23 10:25:06 -0300

    Learning about 'Casting' 23/09/2022

---
+++ casting/specifyAVariableType.py
@@ -0,0 +1,18 @@
+
+#Constructor functions
+
+#integers
+x = int(666)  #   x = 666
+y = int(6.66) #   y = 6
+z = int("3")  #   z = 3
+
+#floats 
+x = float(666)   #  x = 666.0
+y = float(6.66)  #  y = 6.66
+z = float("6.66")#  z = 6.66
+
+#strings 
+x = str("DV") # z = 'DV' 
+y = str(666)  # y = '666'
+z = str(6.66) # z = '6.66'
+
