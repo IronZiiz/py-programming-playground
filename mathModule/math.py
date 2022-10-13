0f27fada8621a9f64a279af148ffab37a3069bf9
IronZiiz <thiagozilli10@gmail.com>
2022-10-13 09:10:49 -0300

    math module

---
+++ mathModule/math.py
@@ -0,0 +1,42 @@
+def hyn():
+    print("------------")
+
+x = min(6, 66, 666)
+y = max(6, 66, 666)
+
+print(x)
+print(y)
+hyn()
+
+x = abs(-6.66)
+print(x)
+hyn()
+
+x = pow(6,6)
+print(x)
+hyn()
+
+# math module
+import math
+
+x = math.sqrt(666)
+print(x)
+hyn()
+
+# math.ceil() method rounds a number upwards to its nearest integer
+# math. floor() method rouds a number downwards to its nearest integer
+
+import math 
+x = math.ceil(6.4)
+y = math.floor(6.4)
+
+print(x)
+print(y)
+hyn()
+
+import math 
+x = math.pi 
+print(x)
+
+
+
