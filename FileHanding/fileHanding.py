aae6f12661ee365188883a2d16b32c94e06a4417
IronZiiz <thiagozilli10@gmail.com>
2022-10-25 22:06:40 -0300

    Next step in python. Learning about filehanding 25/10/2022

---
+++ FileHanding/fileHanding.py
@@ -0,0 +1,12 @@
+# using open() function 
+    # have a lot methods: read, append, write, create
+    # specify fil: binary or text mode 
+        # use t(text) b(binary)
+
+
+# Syntax
+tz = open("/home/ziiz/Documents/Programming/learningPhython/FileHanding/first.txt","rt")
+print(tz.read())
+
+def hyn():
+    print("------------")
