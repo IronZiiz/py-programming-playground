6dc09dbf3c3fe303e14fbbdf5fe9c081cb6de5a4
IronZiiz <thiagozilli10@gmail.com>
2022-11-02 17:49:37 -0300

    Starting study in numpy module

---
+++ NumpyTutorial/numpyGettingStarted.py
@@ -0,0 +1,20 @@
+def hyn():
+    print("------------")
+
+# First : Install numpy
+    # install numpy
+
+import numpy 
+
+firstArray = numpy.array([6,66,666])
+print(firstArray)
+hyn()
+
+import numpy as np 
+
+secArray = np.array([666,6666,66666])
+print(secArray)
+hyn()
+
+# My version NumPy
+print(np.__version__)
